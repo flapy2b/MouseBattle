2e212bd62b1aee23ab495be8817fedf216151400
Gabriel <gabrielmartelligueniot@gmail.com>
2022-11-05 21:27:49 +0100

    Fix the no usename bug

--- public/index.js
+++ public/index.js
@@ -20,3 +20,12 @@ $(document).ready(function () {
 	})
 
 });
+
+$(document).ready(function() {
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
+});
